82d78452972c7992561791cd221bbcdb5ba1bbff
Louis Lam <louislam@users.noreply.github.com>
2023-09-25 19:50:57 +0800

    Init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Bun for Windows via Docker Desktop
+
+A temporary solution for running Bun on Windows. In case you don't have WSL or don't want to use it.
+
+## Installation
+
+Required tools:
+- [Docker Desktop](https://www.docker.com/products/docker-desktop)
+- [Node.js](https://nodejs.org/en/download/)
+
+```bash
+npm install bun-windows-via-docker -g
+```
+
+## Usage
+
+1. Make sure Docker Desktop is running.
+2. Since bun
+
+Use like a regular Bun CLI:
+
+Example:
+
+```bash
+bun index.ts
+```
---
+++ cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import childProcess from "child_process";
+
+// copy process.argv into a new array
+let args = process.argv.slice(0);
+
+// Remove first two arguments ("node" and "cli.js")
+args.shift();
+args.shift();
+
+// Image and tag
+let tag = "latest";
+let image  = "oven/bun";
+
+if (process.env.BUN_DOCKER_TAG) {
+    tag = process.env.BUN_DOCKER_TAG;
+}
+
+if (process.env.BUN_DOCKER_IMAGE) {
+    image = process.env.BUN_DOCKER_IMAGE;
+}
+
+childProcess.spawn("docker", [
+    "run",
+    "--init",
+    "--rm",
+    "--ulimit", "memlock=-1:-1",
+    "-v", process.cwd() + ":/home/bun/app",
+    image + ":" + tag,
+    ... args,
+], {
+    stdio: "inherit",
+});
+
